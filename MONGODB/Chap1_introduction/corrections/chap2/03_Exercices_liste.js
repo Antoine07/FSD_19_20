57b1cc7dafbaf47ce3ded73f44d5d647a9394f00
J.M. Cléry <jmclery@gmail.com>
2022-03-14 15:24:21 +0100

    Adds Corrections for Chap1

---
+++ MONGODB/Chap1_introduction/corrections/chap2/03_Exercices_liste.js
@@ -0,0 +1,20 @@
+//  Liste d'Exercices
+/**
+ * 01. Combien y a t il de restaurants qui font de la cuisine italienne et qui ont eu un score de 10 au moins ?
+ */
+
+ db.restaurants.find({ cuisine: "Italian", "grades.score": 10 }).count(); // 378
+ // Ou (selon l'interpretation de l'exo) :
+ db.restaurants.find({ cuisine: "Italian", "grades.score": { $gte: 10} }).count(); // 982
+
+
+ /**
+  * Affichez également le nom, les scores et les coordonnées GPS de ces restaurants. Ordonnez les résultats par ordre décroissant sur les noms des restaurants.
+  */
+ 
+ db.restaurants
+   .find(
+     { cuisine: "Italian", "grades.score": 10 },
+     { "grades.score": 1, "address.coord": 1, name: 1, _id: 0 }
+   )
+   .sort({ name: -1 })
