d6a89e606b9f7e8644ff9f5547ba27ad8262263d
William Rao Fernandes <williamraofernandes@gmail.com>
2022-03-18 13:43:42 +0100

    modif fonctions mongo server.js

--- MONGODB/Challenge/MODELS/server.js
+++ MONGODB/Challenge/MODELS/server.js
@@ -16,69 +16,79 @@ const client = new MongoClient(uri);
 
 // Async pour attendre la connexion avant de faire quoique se soit
 async function requestMongo(
-  restriction = {},
-  proj = { _id: 0, name: 1 },
-  limit = 10) {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
+    restriction,
+    proj,
+    limit) {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected successfully to server");
 
-    const db = await client.db(dbName);
-    const collection = await db.collection(collectionName);
-    // attention à la syntaxe pour faire votre requête
-    const docs = await collection.find(restriction, {
-      projection: proj
-    }).limit(limit).toArray();
+        const db = await client.db(dbName);
+        const collection = await db.collection(collectionName);
+        // attention à la syntaxe pour faire votre requête
+        if (limit != 0) {
+            const docs = await collection.find(restriction, {
+                projection: proj
+            }).limit(limit).toArray();
+        } else {
+            const docs = await collection.find(restriction, {
+                projection: proj
+            });
+        }
+        const docs = await collection.find(restriction, {
+            projection: proj
+        }).limit(limit).toArray();
 
-    return docs;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-    console.log('disconnect');
-  }
+        return docs;
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+        console.log('disconnect');
+    }
 }
 
-async function requestMongoAggregate() {
-  try {
-    await client.connect();
-    const db = await client.db(dbName);
-    const collection = await db.collection(collectionName);
+async function requestMongoAggregate(pipeline) {
+    try {
+        await client.connect();
+        const db = await client.db(dbName);
+        const collection = await db.collection(collectionName);
 
-    const pipeline = [
-      { $match: { borough: "Bronx" } },
-      { $group: { _id: "$cuisine", count: { $sum: 1 } } }
-    ];
-    const aggCursor = await collection.aggregate(pipeline).toArray()
 
-    return aggCursor;
-  } finally {
-    await client.close();
-    console.log('disconnect');
-  }
+        const aggCursor = await collection.aggregate(pipeline).toArray()
+
+        return aggCursor;
+    } finally {
+        await client.close();
+        console.log('disconnect');
+    }
 }
 
 app.use(express.json())
 app.get("/", (req, res) => {
-  requestMongo()
-    .then(docs => res.render('index', { restaurants: docs }))
-    .catch(err => res.send({ err }));
+    requestMongo({ "borough": "Brooklyn" }, { _id: 0, name: 1 }, 10)
+        .then(docs => res.render('index', { restaurants: docs }))
+        .catch(err => res.send({ err }));
 });
 
-app.get("/best", (req, res) => {
-  requestMongo()
-    .then(docs => res.render('index', { restaurants: docs }))
-    .catch(err => res.send({ err }));
+app.get("/Brooklyn", (req, res) => {
+    requestMongo({ "borough": "Brooklyn" }, { _id: 0, name: 1 }, 0)
+        .then(docs => res.render('index', { restaurants: docs }))
+        .catch(err => res.send({ err }));
 });
+
 
 app.get('/a', (req, res) => {
-  return res.send({ a : 1 })
-  requestMongoAggregate.then(docs => {
-    res.send({docs})
-  }).catch(err => res.send({err}))
+    //return res.send({ a : 1 })
+    requestMongoAggregate([
+        { $match: { borough: "Bronx" } },
+        { $group: { _id: "$cuisine", count: { $sum: 1 } } }
+    ]).then(docs => {
+        res.send({ docs })
+    }).catch(err => res.send({ err }))
 
 })
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 });
